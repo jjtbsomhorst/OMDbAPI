b3abf46d5b2e6a22b79bccbf699357cfc3c14388
j.somhorst@gmail.com <MEdHnz12g9YobnWakqgT>
2020-07-28 18:48:11 +0200

    Initial development branche

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$autoload = __DIR__.'/../vendor/autoload.php';
-if (!file_exists($autoload)) {
-    die('No vendor autoload (did you composer install?)');
-}
-$loader = include($autoload);
-$loader->add('aharen', __DIR__.'/../src/');
-
